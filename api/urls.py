dbeb657b5b4a7fe18701a62c06144bf8e6a53297
Zubair Chowdhury <zubairchowdhury12@hotmail.co.uk>
2017-10-08 11:40:31 +0100

    Modified import statement to not be relative

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
 
-from . import views
+from api.views import translate
 
 urlpatterns = [
-       url(r'^test/?$', views.translate)
+       url(r'^test/?$', translate)
 ]
